548e04f9c2448dc3716964f10b5f18f03e39e6cc
Dhatri <dhatri.venuturla@gmail.com>
2024-09-28 05:11:57 +0530

    updated output.tf file

--- output.tf
+++ output.tf
@@ -1,3 +1,23 @@
 output "vpc_id" {
   value = aws_vpc.main.id
 }
+
+output "public_sb_ids" {
+  value = aws_subnet.public[*].id
+}
+
+output "private_sb_ids" {
+  value = aws_subnet.private[*].id
+}
+
+output "database_sb_ids" {
+  value = aws_subnet.database[*].id
+}
+
+output "database_group_name" {
+  value = aws_db_subnet_group.default.name
+}
+
+output "database_group_id" {
+  value = aws_db_subnet_group.default.id
+}
